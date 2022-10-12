3587e2a90708b0c072bba296bc260c849dd04cd6
Adam Babik <a.babik@designfortress.com>
2022-10-12 15:33:14 +0200

    Unify data structures in cli and renderer

--- internal/document/source.go
+++ internal/document/source.go
@@ -3,6 +3,7 @@ package document
 import (
 	"fmt"
 	"io/fs"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/yuin/goldmark"
@@ -90,6 +91,122 @@ func (s *ParsedSource) CodeBlocks() CodeBlocks {
 	}
 
 	return result
+}
+
+func getRange(source []byte, n ast.Node, start int, stop int) (string, error) {
+	var content strings.Builder
+	switch n.Kind() {
+	case ast.KindHeading:
+		heading := n.(*ast.Heading)
+		offset := 1 + heading.Level
+		// shield from inital ======= vs ### heading
+		if start-offset < 0 {
+			offset = 0
+		}
+		_, _ = content.Write(source[start-offset : stop])
+	default:
+		_, _ = content.Write(source[start:stop])
+	}
+	return content.String(), nil
+}
+
+func getPrevStart(n ast.Node) int {
+	curr := n
+	prev := n.PreviousSibling()
+	if prev != nil && prev.Lines().Len() > 0 {
+		curr = prev
+	}
+	return curr.Lines().At(0).Stop
+}
+
+func getNextStop(n ast.Node) int {
+	curr := n
+	next := n.NextSibling()
+	if next != nil {
+		curr = next
+	}
+
+	l := curr.Lines().Len()
+	if l == 0 {
+		return 0
+	}
+
+	stop := curr.Lines().At(l - 1).Start
+
+	// add back markdown heading levels
+	if curr.Kind() == ast.KindHeading {
+		heading := curr.(*ast.Heading)
+		// simple math to add back ## incl trailing space
+		stop = stop - 1 - heading.Level
+	}
+
+	return stop
+}
+
+func (s *ParsedSource) SquashedBlocks() (blocks Blocks, err error) {
+	nameRes := &nameResolver{
+		namesCounter: map[string]int{},
+		cache:        map[interface{}]string{},
+	}
+
+	lastCodeBlock := s.root
+	remainingNode := s.root
+
+	err = ast.Walk(s.root, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
+		if n.Kind() != ast.KindFencedCodeBlock || !entering {
+			return ast.WalkContinue, nil
+		}
+
+		start := getPrevStart(n)
+
+		if lastCodeBlock != nil {
+			stop := getNextStop(lastCodeBlock)
+			// check for existence of markdown in between code blocks
+			if start > stop {
+				markdown, _ := getRange(s.data, n, stop, start)
+				blocks = append(blocks, &MarkdownBlock{content: markdown})
+			}
+			lastCodeBlock = n
+		}
+
+		blocks = append(blocks, &CodeBlock{
+			source:       s.data,
+			inner:        n.(*ast.FencedCodeBlock),
+			nameResolver: nameRes,
+		})
+
+		remainingNode = n.NextSibling()
+
+		return ast.WalkContinue, nil
+	})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	// Never encounter a code block, stuck on document node
+	if remainingNode == s.root {
+		remainingNode = remainingNode.FirstChild()
+	}
+
+	// Skip remainingNodes unless it's got lines
+	for remainingNode != nil && remainingNode.Lines().Len() == 0 {
+		remainingNode = remainingNode.NextSibling()
+	}
+
+	if remainingNode != nil {
+		start := remainingNode.Lines().At(0).Start
+		stop := len(s.data) - 1
+		markdown, _ := getRange(s.data, remainingNode, start, stop)
+		blocks = append(blocks, &MarkdownBlock{content: markdown})
+	}
+
+	if len(blocks) == 2 {
+		if _, ok := blocks[0].(*MarkdownBlock); ok {
+			blocks = blocks[1:]
+		}
+	}
+
+	return
 }
 
 type defaultParser struct {
