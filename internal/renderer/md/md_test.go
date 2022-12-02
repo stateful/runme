bfa038e0b9897db0357f80ff3f33aac76b8e9e2f
Adam Babik <a.babik@designfortress.com>
2022-12-02 10:45:29 +0100

    Use go:embed in renderer tests

--- internal/renderer/md/md_test.go
+++ internal/renderer/md/md_test.go
@@ -1,12 +1,11 @@
 package md_test
 
 import (
-	"io"
 	"io/fs"
-	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/stateful/runme/internal/renderer"
 	"github.com/stateful/runme/internal/renderer/md"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -81,7 +80,7 @@ cluster-create-std
 }
 
 func TestRender_Testdata(t *testing.T) {
-	err := filepath.Walk("../testdata", func(path string, info fs.FileInfo, err error) error {
+	err := fs.WalkDir(renderer.TestContent, ".", func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -89,9 +88,7 @@ func TestRender_Testdata(t *testing.T) {
 			return nil
 		}
 		t.Run(filepath.Base(path), func(t *testing.T) {
-			f, err := os.Open(path)
-			require.NoError(t, err)
-			data, err := io.ReadAll(f)
+			data, err := fs.ReadFile(renderer.TestContent, path)
 			require.NoError(t, err)
 			testEquality(t, data)
 		})
---
+++ internal/renderer/testdata.go
@@ -0,0 +1,6 @@
+package renderer
+
+import "embed"
+
+//go:embed testdata
+var TestContent embed.FS
