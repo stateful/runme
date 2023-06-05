37ac4fc132628f8aa4c904043abcd4258158aae1
Christian Bromann <git@bromann.dev>
2023-06-05 12:45:12 +0200

    Have a more verbose error message for Windows environment

--- internal/runner/command_windows.go
+++ internal/runner/command_windows.go
@@ -14,7 +14,10 @@ func setSysProcAttrCtty(cmd *exec.Cmd) {}
 func setSysProcAttrPgid(cmd *exec.Cmd) {}
 
 func disableEcho(fd uintptr) error {
-	return errors.New("unsupported")
+	return errors.New("Error: Environtment not supported! " +
+		"Runme currently doesn't support Window shells. " +
+		"Please go to https://github.com/stateful/runme/issues/173 to follow progress on this " +
+		"and join our Discord server at https://discord.gg/runme if you have further questions!")
 }
 
 func signalPgid(pid int, sig os.Signal) error { return errors.New("unsupported") }
