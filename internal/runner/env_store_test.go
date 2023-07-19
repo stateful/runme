43d0a2ba9bd369cde316cbca505941a769905480
Max Stoumen <max@mxs.dev>
2023-07-19 13:33:20 -0700

    internal/runner: limit env store size (#339)
    * limit env store size
    
    * ensure size contribution is contributed
    
    * unit test

--- internal/runner/env_store_test.go
+++ internal/runner/env_store_test.go
@@ -1,3 +1,54 @@
 package runner
 
-// TODO: add unit tests
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO: add more unit tests
+
+func Test_maxEnvSize(t *testing.T) {
+	{
+		session := newEnvStore()
+
+		_, err := session.Set("key", strings.Repeat("a", maxEnvSize))
+		assert.Error(t, err)
+	}
+
+	{
+		session := newEnvStore()
+
+		_, err := session.Set("key", "value")
+		assert.NoError(t, err)
+	}
+
+	{
+		session := newEnvStore()
+
+		{
+			_, err := session.Set("key", strings.Repeat("a", maxEnvSize/2+1))
+			assert.NoError(t, err)
+		}
+
+		{
+			_, err := session.Set("key", strings.Repeat("a", (maxEnvSize/2)+1))
+			assert.NoError(t, err)
+		}
+	}
+
+	{
+		session := newEnvStore()
+
+		{
+			_, err := session.Set("key", strings.Repeat("a", maxEnvSize/2+1))
+			assert.NoError(t, err)
+		}
+
+		{
+			_, err := session.Set("key2", strings.Repeat("a", (maxEnvSize/2)+1))
+			assert.Error(t, err)
+		}
+	}
+}
