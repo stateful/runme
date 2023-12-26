373368876207d995959b20b4e87578fd900765b7
Adam Babik <a.babik@designfortress.com>
2023-12-26 09:44:54 +0100

    Enable profiling using RUNME_PROFILE_{CPU,MEM}

--- main.go
+++ main.go
@@ -3,21 +3,67 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/stateful/runme/internal/cmd"
 	"github.com/stateful/runme/internal/version"
 )
 
-func root() int {
-	root := cmd.Root()
-	root.Version = fmt.Sprintf("%s (%s) on %s", version.BuildVersion, version.Commit, version.BuildDate)
-	if err := root.Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err.Error())
-		return 1
-	}
-	return 0
-}
-
 func main() {
 	os.Exit(root())
+}
+
+func root() (status int) {
+	root := cmd.Root()
+	root.Version = fmt.Sprintf("%s (%s) on %s", version.BuildVersion, version.Commit, version.BuildDate)
+
+	rootWithCPUProfile(func() {
+		if err := root.Execute(); err != nil {
+			logf("could not execute command: %v", err)
+			status = 1
+		}
+	})
+
+	return
+}
+
+func rootWithCPUProfile(fn func()) {
+	if profile := os.Getenv("RUNME_PROFILE_CPU"); profile != "" {
+		f, err := os.Create(profile)
+		if err != nil {
+			fatalf("could not create CPU profile: %v", err)
+		}
+		defer f.Close()
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			fatalf("could not start CPU profile: %v", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	fn()
+
+	if profile := os.Getenv("RUNME_PROFILE_MEM"); profile != "" {
+		f, err := os.Create(profile)
+		if err != nil {
+			fatalf("could not create mem profile: %v", err)
+		}
+		defer f.Close()
+
+		runtime.GC()
+
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			fatalf("could not write heap profile: %v", err)
+		}
+	}
+}
+
+func fatalf(format string, args ...interface{}) {
+	logf(format, args...)
+	os.Exit(1)
+}
+
+func logf(format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(os.Stderr, format, args...)
 }
