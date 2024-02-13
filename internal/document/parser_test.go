53036394572826b33d6b3aaa28fbcb5d36836562
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-02-13 18:06:33 -0500

    Skip preprocessor includes, they are not JSON

--- internal/document/parser_test.go
+++ internal/document/parser_test.go
@@ -79,3 +79,16 @@ A paragraph
 	assert.Equal(t, "{\n    \"prop1\": \"val1\",\n    \"prop2\": \"val2\"\n}", string(sections.FrontMatter))
 	assert.Equal(t, "# Example\n\nA paragraph\n", string(sections.Content))
 }
+
+func TestParseSections_SkipIncludes(t *testing.T) {
+	data := []byte(`{% include "gradual-rollout-intro.md" %}
+
+## Procedure
+
+You can configure the rollout-duration parameter by modifying the config-network ConfigMap, or by using the Operator.
+`)
+	sections, err := ParseSections(data)
+	require.NoError(t, err)
+	assert.Equal(t, "", string(sections.FrontMatter))
+	assert.Equal(t, "{% include \"gradual-rollout-intro.md\" %}\n\n## Procedure\n\nYou can configure the rollout-duration parameter by modifying the config-network ConfigMap, or by using the Operator.\n", string(sections.Content))
+}
