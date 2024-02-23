ec8213e7c11edf475162e10e1afaea65049a2809
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-02-23 11:04:03 -0500

    Expand tests

--- internal/command/env_resolver_test.go
+++ internal/command/env_resolver_test.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -9,67 +10,91 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type parsingFixture struct {
+	name   string
+	data   string
+	source []EnvResolverSource
+	result *EnvResolverResult
+}
+
+func runEnvResolverParsingTestCase(tc parsingFixture, mode EnvResolverMode) func(t *testing.T) {
+	return func(t *testing.T) {
+		r := NewEnvResolver(mode, tc.source...)
+
+		if mode != EnvResolverModeAuto && tc.result != nil {
+			tc.result.Prompt = EnvResolverResolved
+			tc.result.Value = tc.result.OriginalValue
+		}
+
+		var script bytes.Buffer
+		result, err := r.Resolve(strings.NewReader(tc.data), &script)
+		assert.NoError(t, err)
+
+		var tcres []*EnvResolverResult
+		if tc.result != nil {
+			tcres = append(tcres, tc.result)
+		}
+
+		assert.EqualValues(t, tcres, result)
+	}
+}
+
 func TestEnvResolverParsing(t *testing.T) {
-	testCases := []struct {
-		name   string
-		data   string
-		source []EnvResolverSource
-		result []*EnvResolverResult
-	}{
+	fixtures := []parsingFixture{
 		{
 			name: "no value",
 			data: `export TEST_NO_VALUE`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_NO_VALUE", Prompt: EnvResolverMessage},
+			result: &EnvResolverResult{
+				Name: "TEST_NO_VALUE", Prompt: EnvResolverMessage,
 			},
 		},
 		{
 			name: "empty value",
 			data: `export TEST_EMPTY_VALUE=`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_EMPTY_VALUE", Prompt: EnvResolverMessage},
+			result: &EnvResolverResult{
+				Name: "TEST_EMPTY_VALUE", Prompt: EnvResolverMessage,
 			},
 		},
 		{
 			name: "string value",
 			data: `export TEST_STRING_VALUE=value`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_VALUE", OriginalValue: "value", Prompt: EnvResolverMessage},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_VALUE", OriginalValue: "value", Prompt: EnvResolverMessage,
 			},
 		},
 		{
 			name: "string value with equal sign",
 			data: `export TEST_STRING_VALUE_WITH_EQUAL_SIGN=part1=part2`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_VALUE_WITH_EQUAL_SIGN", OriginalValue: "part1=part2", Prompt: EnvResolverMessage},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_VALUE_WITH_EQUAL_SIGN", OriginalValue: "part1=part2", Prompt: EnvResolverMessage,
 			},
 		},
 		{
 			name: "string double quoted value empty",
 			data: `export TEST_STRING_DBL_QUOTED_VALUE_EMPTY=""`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_DBL_QUOTED_VALUE_EMPTY", OriginalValue: "", Prompt: EnvResolverMessage},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_DBL_QUOTED_VALUE_EMPTY", OriginalValue: "", Prompt: EnvResolverMessage,
 			},
 		},
 		{
 			name: "string double quoted value",
 			data: `export TEST_STRING_DBL_QUOTED_VALUE="value"`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_DBL_QUOTED_VALUE", OriginalValue: "value", Prompt: EnvResolverPlaceholder},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_DBL_QUOTED_VALUE", OriginalValue: "value", Prompt: EnvResolverPlaceholder,
 			},
 		},
 		{
 			name: "string single quoted value empty",
 			data: `export TEST_STRING_SGL_QUOTED_VALUE_EMPTY=''`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_SGL_QUOTED_VALUE_EMPTY", OriginalValue: "", Prompt: EnvResolverPlaceholder},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_SGL_QUOTED_VALUE_EMPTY", OriginalValue: "", Prompt: EnvResolverPlaceholder,
 			},
 		},
 		{
 			name: "string single quoted value",
 			data: `export TEST_STRING_SGL_QUOTED_VALUE='value'`,
-			result: []*EnvResolverResult{
-				{Name: "TEST_STRING_SGL_QUOTED_VALUE", OriginalValue: "value", Prompt: EnvResolverPlaceholder},
+			result: &EnvResolverResult{
+				Name: "TEST_STRING_SGL_QUOTED_VALUE", OriginalValue: "value", Prompt: EnvResolverPlaceholder,
 			},
 		},
 		{
@@ -89,14 +114,18 @@ func TestEnvResolverParsing(t *testing.T) {
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			r := NewEnvResolver(EnvResolverModeAuto, tc.source...)
-			var script bytes.Buffer
-			result, err := r.Resolve(strings.NewReader(tc.data), &script)
-			assert.NoError(t, err)
-			assert.EqualValues(t, tc.result, result)
-		})
+	for _, tc := range fixtures {
+		t.Run(
+			fmt.Sprintf("Auto_resolve %s", tc.name),
+			runEnvResolverParsingTestCase(tc, EnvResolverModeAuto),
+		)
+	}
+
+	for _, tc := range fixtures {
+		t.Run(
+			fmt.Sprintf("Skip_resolve %s", tc.name),
+			runEnvResolverParsingTestCase(tc, EnvResolverModeSkip),
+		)
 	}
 }
 
@@ -105,7 +134,10 @@ func TestEnvResolverResolve(t *testing.T) {
 	var script bytes.Buffer
 	result, err := r.Resolve(strings.NewReader(`export MY_ENV=default`), &script)
 	require.NoError(t, err)
-	require.EqualValues(t, []*EnvResolverResult{
-		{Name: "MY_ENV", OriginalValue: "default", Value: "resolved", Prompt: EnvResolverResolved},
+	require.EqualValues(t, &EnvResolverResult{
+		Name:          "MY_ENV",
+		OriginalValue: "default",
+		Value:         "resolved",
+		Prompt:        EnvResolverResolved,
 	}, result)
 }
