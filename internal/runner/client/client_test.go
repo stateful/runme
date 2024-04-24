9a4ab02f5985974686ef0920f990e0875afe53a0
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2024-04-24 17:06:36 -0400

    Includes remote runner tests

--- internal/runner/client/client_test.go
+++ internal/runner/client/client_test.go
@@ -2,14 +2,20 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"path/filepath"
 	"runtime"
 	"testing"
 
+	"github.com/oklog/ulid/v2"
 	runnerv1 "github.com/stateful/runme/v3/internal/gen/proto/go/runme/runner/v1"
 	"github.com/stateful/runme/v3/internal/project"
+	"github.com/stateful/runme/v3/internal/runner"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 )
 
 func TestResolveDirectory(t *testing.T) {
@@ -64,11 +70,48 @@ func TestResolveDirectory(t *testing.T) {
 	}
 }
 
+func testStartRunnerServiceServer(t *testing.T) (
+	net.Listener,
+	func(),
+) {
+	logger, err := zap.NewDevelopment()
+	require.NoError(t, err)
+
+	lis, err := net.Listen("unix", fmt.Sprintf("/tmp/sock-%s.sock", ulid.Make()))
+	require.NoError(t, err)
+	server := grpc.NewServer()
+
+	rss, err := runner.NewRunnerService(logger)
+	require.NoError(t, err)
+
+	runnerv1.RegisterRunnerServiceServer(server, rss)
+	go server.Serve(lis)
+
+	return lis, server.Stop
+}
+
 func TestResolveProgramLocal(t *testing.T) {
+	list, stop := testStartRunnerServiceServer(t)
+	t.Cleanup(stop)
+	// conn, _ := testCreateRunnerServiceClient(t, lis)
+
 	type Var struct {
 		Status runnerv1.ResolveProgramResponse_Status
 		Name   string
 	}
+
+	runnerOpts := []RunnerOption{
+		WithInsecure(true),
+	}
+	localRunner, err := NewLocalRunner(runnerOpts...)
+	assert.NoError(t, err)
+
+	remoteRunner, err := NewRemoteRunner(
+		context.Background(),
+		fmt.Sprintf("unix://%s", list.Addr().String()),
+		runnerOpts...,
+	)
+	assert.NoError(t, err)
 
 	testCases := []struct {
 		Title          string
@@ -127,27 +170,33 @@ func TestResolveProgramLocal(t *testing.T) {
 		},
 	}
 
-	runnerOpts := []RunnerOption{}
-
-	runner, err := NewLocalRunner(runnerOpts...)
-	assert.NoError(t, err)
-
 	for _, tt := range testCases {
 		t.Run(tt.Title, func(t *testing.T) {
-			ctx := context.Background()
-			resp, err := runner.ResolveProgram(ctx, tt.Mode, tt.Input)
+			for _, r := range []struct {
+				name   string
+				runner Runner
+			}{
+				{"local", localRunner},
+				{"remote", remoteRunner},
+			} {
+				t.Run(r.name, func(t *testing.T) {
+					ctx := context.Background()
+					resp, err := r.runner.ResolveProgram(ctx, tt.Mode, tt.Input)
+					require.NoError(t, err)
 
-			var vars []Var
-			for _, v := range resp.Vars {
-				vars = append(vars, Var{
-					Status: v.Status,
-					Name:   v.Name,
+					var vars []Var
+					for _, v := range resp.Vars {
+						vars = append(vars, Var{
+							Status: v.Status,
+							Name:   v.Name,
+						})
+					}
+
+					assert.NoError(t, err)
+					assert.Equal(t, tt.ExpectedScript, resp.Script)
+					assert.Equal(t, tt.ExpectedVars, vars)
 				})
 			}
-
-			assert.NoError(t, err)
-			assert.Equal(t, tt.ExpectedScript, resp.Script)
-			assert.Equal(t, tt.ExpectedVars, vars)
 		})
 	}
 }
