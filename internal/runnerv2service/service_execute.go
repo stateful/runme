069d7ff8fa19ff958c4d44a9dff3fc9d9d94983c
Jeremy Lewi <jeremy@lewi.us>
2025-03-15 13:19:05 -0700

    Draft.

--- internal/runnerv2service/service_execute.go
+++ internal/runnerv2service/service_execute.go
@@ -140,7 +140,103 @@ func (r *runnerService) Execute(srv runnerv2.RunnerService_ExecuteServer) error 
 	return waitErr
 }
 
+func (r *runnerService) ExecuteOneShot(srv runnerv2.RunnerService_ExecuteOneShotServer) error {
+	runID := ulid.GenerateID()
+	logger := r.logger.Named("Execute").With(zap.String("id", runID))
+
+	// Get the initial request.
+	req := &runnerv2.ExecuteOneShotRequest{}
+	if err := srv.RecvMsg(req); err != nil {
+		if errors.Is(err, io.EOF) {
+			logger.Info("client closed the connection while getting initial request; exiting")
+			return nil
+		}
+		logger.Info("failed to receive a request", zap.Error(err))
+		return errors.WithStack(err)
+	}
+	logger.Info("received request", zap.Any("req", req))
+
+	execInfo := getExecutionInfoFromOneShotExecutionRequest(req)
+	execInfo.RunID = runID
+
+	ctx := rcontext.WithExecutionInfo(srv.Context(), execInfo)
+
+	// Load the project.
+	// TODO(adamb): this should come from the runme.yaml in the future.
+	proj, err := convertProtoProjectToProject(req.GetProject())
+	if err != nil {
+		return err
+	}
+
+	// Manage the session.
+	session, existed, err := r.getOrCreateSessionFromRequest(req, proj)
+	if err != nil {
+		return err
+	}
+	if !existed {
+		err := r.sessions.Add(session)
+		if err != nil {
+			return err
+		}
+	}
+	if err := session.SetEnv(ctx, req.Config.Env...); err != nil {
+		return err
+	}
+
+	exec, err := newExecution(
+		req.Config,
+		proj,
+		session,
+		logger,
+		req.StoreStdoutInEnv,
+	)
+	if err != nil {
+		return err
+	}
+
+	// Start the command and send the initial response with PID.
+	if err := exec.Cmd.Start(ctx); err != nil {
+		return err
+	}
+	if err := srv.Send(&runnerv2.ExecuteOneShotResponse{
+		Pid: &wrapperspb.UInt32Value{Value: uint32(exec.Cmd.Pid())},
+	}); err != nil {
+		return err
+	}
+
+	if err := exec.SetWinsize(req.Winsize); err != nil {
+		logger.Info("failed to set winsize; ignoring", zap.Error(err))
+	}
+
+	if _, err := exec.Write(req.InputData); err != nil {
+		logger.Info("failed to write to stdin; ignoring", zap.Error(err))
+	}
+	exitCode, waitErr := exec.Wait(ctx, srv)
+	logger.Info("command finished", zap.Int("exitCode", exitCode), zap.Error(waitErr))
+
+	var finalExitCode *wrapperspb.UInt32Value
+	if exitCode > -1 {
+		finalExitCode = wrapperspb.UInt32(uint32(exitCode))
+	}
+
+	if err := srv.Send(&runnerv2.ExecuteResponse{
+		ExitCode: finalExitCode,
+	}); err != nil {
+		logger.Info("failed to send exit code", zap.Error(err))
+	}
+
+	return waitErr
+}
+
 func getExecutionInfoFromExecutionRequest(req *runnerv2.ExecuteRequest) *rcontext.ExecutionInfo {
+	return &rcontext.ExecutionInfo{
+		ExecContext: "Execute",
+		KnownID:     req.GetConfig().GetKnownId(),
+		KnownName:   req.GetConfig().GetKnownName(),
+	}
+}
+
+func getExecutionInfoFromOneShotExecutionRequest(req *runnerv2.ExecuteOneShotRequest) *rcontext.ExecutionInfo {
 	return &rcontext.ExecutionInfo{
 		ExecContext: "Execute",
 		KnownID:     req.GetConfig().GetKnownId(),
